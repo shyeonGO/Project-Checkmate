c1d69ab790c59a3594f56e38615853880911d315
SteamB23 <steamb23@outlook.com>
2020-10-26 15:36:46 +0900

    캐릭터 이동 테스트

---
+++ Assets/Game/101. Scripts/PlayerCharacter/PlayerCharacter.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using Cinemachine;
+
+[RequireComponent(typeof(PlayerCharacterControl))]
+[RequireComponent(typeof(Animator))]
+public class PlayerCharacter : MonoBehaviour
+{
+    [SerializeField] CinemachineFreeLook freeLookCamera;
+
+    Transform thisTransform;
+    PlayerCharacterControl characterControl;
+    Animator animator;
+
+    Vector3 moveVelocity;
+
+    #region SmoothDamp
+    #endregion
+
+    public void Awake()
+    {
+        thisTransform = transform;
+        characterControl = GetComponent<PlayerCharacterControl>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void FixedUpdate()
+    {
+        moveVelocity = characterControl.MoveInput;
+
+        var moveMagnitude = moveVelocity.magnitude;
+        if (moveMagnitude > 0.1f)
+        {
+            LookAt(moveVelocity);
+            animator.SetFloat("ySpeed", moveMagnitude);
+        }
+        else
+        {
+            animator.SetFloat("ySpeed", 0);
+        }
+    }
+
+    void LookAt(Vector2 direction)
+    {
+        var angle = Mathf.Atan2(direction.y, -direction.x) * Mathf.Rad2Deg - 90;
+        // 카메라가 바라보는 방향
+        thisTransform.rotation = Quaternion.Euler(0, freeLookCamera.m_XAxis.Value + angle, 0);
+    }
+}
---
+++ Assets/Game/101. Scripts/PlayerCharacter/PlayerCharacterControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerCharacterControl : MonoBehaviour
+{
+    public Vector2 MoveInput { get; private set; }
+    public Vector2 LookInput { get; private set; }
+    public bool SprintInput { get; private set; }
+
+    public void OnMove(InputValue input)
+    {
+        MoveInput = input.Get<Vector2>();
+    }
+
+    public void OnLook(InputValue input)
+    {
+        LookInput = input.Get<Vector2>();
+    }
+
+    public void OnSprint(InputValue input)
+    {
+        SprintInput = input.Get<bool>();
+    }
+}
