d33c3e5f35d50753f3328366583128785a99efa3
lyp3238 <lyp3238@gmail.com>
2020-11-02 22:13:03 +0900

    추가적으로 필요한 임시 리소스 추가 및 AI스크립트 추가

---
+++ Assets/Game/101. Scripts/AIPrototype/AIMaster.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMaster : MonoBehaviour
+{
+    [Header("Basic Setting")]
+    public string bossName;
+    public float healthPoint;
+
+    [Header("Groggy")]
+    public float groggy;
+    private float saveGroggy;
+    public float groggyContinuingTime;
+    public float groggyRecoverySpeed;
+    public float groggyRecoveryInterval;
+
+    [Header("Infect")]
+    public float infect;
+    private float saveInfect;
+    public float infectContinuingTime;
+    public float infectRecoverySpeed;
+    public float infectRecoveryInterval;
+
+    private NavMeshAgent agent;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
+        // 추가됨
+        saveGroggy = 0;
+        saveInfect = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
