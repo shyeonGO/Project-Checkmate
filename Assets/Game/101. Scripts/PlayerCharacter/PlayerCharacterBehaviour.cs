5ac16af8d1e937472557d1a7adc5665e30aa00f5
SteamB23 <steamb23@outlook.com>
2020-11-04 01:03:06 +0900

    이동 보정 및 더미애니메이션 정리

--- Assets/Game/101. Scripts/PlayerCharacter/PlayerCharacterBehaviour.cs
+++ Assets/Game/101. Scripts/PlayerCharacter/PlayerCharacterBehaviour.cs
@@ -1,21 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using Cinemachine;
 
 [RequireComponent(typeof(PlayerCharacterController))]
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerCharacterBehaviour : MonoBehaviour
 {
     #region 인스펙터 변수
-    // 코드에서 이 변수에 직접 접근하는 것은 지양하세요!
+    [Header("Ground")]
+    //[SerializeField] LayerMask layerMask;
+    //[SerializeField] float maxStepHeight = 0.5f;
+    //[SerializeField] float stepSearchOvershoot = 0.01f;
+    [SerializeField] float maxSlope = 45;
+    [Header("SmoothTime")]
     [SerializeField] float characterWorldAngleSmoothTime = 0.1f;
     [SerializeField] float moveVelocitySmoothTime = 0.1f;
+    [Header("Debug")]
+    // 착지 상태
+    [SerializeField] bool isGround = false;
+    // 가파른 상태
+    [SerializeField] bool isSteep = false;
     #endregion
 
 
     Transform thisTransform;
     PlayerCharacterController characterControl;
     Animator animator;
+    Rigidbody rigidbody;
 
     Transform mainCameraTransform;
 
@@ -25,29 +38,108 @@ public class PlayerCharacterBehaviour : MonoBehaviour
     Vector2 moveVelocity;
     Vector2 moveVelocitySmooth;
 
-    public void Awake()
+    Vector3 currentClimbDirection;
+
+    List<ContactPoint> contactPoints = new List<ContactPoint>(0);
+
+    void Awake()
     {
         thisTransform = transform;
         characterControl = GetComponent<PlayerCharacterController>();
         animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
 
         mainCameraTransform = Camera.main.transform;
 
         var forward = thisTransform.forward;
 
-        characterWorldAngle = characterWorldAngleTarget = Mathf.Atan2(forward.y, -forward.x) * Mathf.Rad2Deg - 0;
+        characterWorldAngle = characterWorldAngleTarget = thisTransform.rotation.eulerAngles.y;
     }
 
-    public void FixedUpdate()
+    void FixedUpdate()
     {
-        var moveVelocityRaw = characterControl.MoveInput;
+        GroundUpdate();
+        MoveUpdate();
+    }
+
+    Vector3 lastVelocity;
+    void GroundUpdate()
+    {
+        isGround = FindGround(out var groundCP, contactPoints);
+        animator.SetBool("isGround", isGround);
+
+        if (isGround)
+        {
+            currentClimbDirection = Vector3.Cross(thisTransform.right, groundCP.normal);
+            //Debug.Log(currentClimbDirection);
+
+            //animator.applyRootMotion = true;
+
+            // slope
+            isSteep = currentClimbDirection.y > Mathf.Cos(maxSlope * Mathf.Deg2Rad);
+
+            var moveSpeed = animator.velocity.magnitude;
+            Debug.Log($"{moveSpeed:n5}");
+
+            if (!isSteep && moveSpeed > 0.1f)
+            {
+                if (currentClimbDirection.y < 0)
+                {
+                    // 위에서 아래로 누르기
+                    rigidbody.velocity = new Vector3(0, Mathf.Tan(currentClimbDirection.y) * 1f * moveSpeed, 0);
+                }
+                else
+                {
+                    // 아래에서 위로 올리기
+                    rigidbody.velocity = new Vector3(0, Mathf.Tan(currentClimbDirection.y) * 0.8f * moveSpeed, 0);
+                }
+            }
+            //if (FindStep(out var stepUpOffset, contactPoints, groundCP))
+            //{
+            //    rigidbody.position += stepUpOffset;
+            //    rigidbody.velocity = lastVelocity;
+            //}
+
+            //var velocity = rigidbody.velocity;
+            //if (moveVelocity.sqrMagnitude > 0.0001f)
+            //{
+            //    var forwardPosition = rigidbody.position + thisTransform.forward * 0.5f;
+            //    var stepHeightVector = Vector3.up * maxStepHeight;
+            //    if (Physics.Raycast(forwardPosition + stepHeightVector, Vector3.down, out var hit, maxStepHeight * 2))
+            //    {
+            //        velocity.y = (hit.point - rigidbody.position).y;
+            //        velocity.y *= 4.5f;
+            //        rigidbody.velocity = velocity;
+            //        Debug.Log($"{velocity.y:n5}");
+            //    }
+            //}
+        }
+
+        if(isGround && !isSteep)
+        {
+            animator.applyRootMotion = true;
+        }
+        else
+        {
+            animator.applyRootMotion = false;
+        }
+
+        contactPoints.Clear();
+
+        lastVelocity = rigidbody.velocity;
+    }
+
+    void MoveUpdate()
+    {
+        var moveVelocityRaw = isGround ? characterControl.MoveInput : Vector2.zero;
         moveVelocity = Vector2.SmoothDamp(moveVelocity, moveVelocityRaw, ref moveVelocitySmooth, moveVelocitySmoothTime);
+
 
         var moveMagnitude = moveVelocity.magnitude;
         var moveRawMagnitude = moveVelocityRaw.magnitude;
         if (moveRawMagnitude > 0.1f)
         {
-            LookAt(moveVelocityRaw);
+            LookAtByCamera(moveVelocityRaw);
             animator.SetFloat("ySpeed", moveMagnitude);
             animator.SetBool("isMove", true);
         }
@@ -60,12 +152,106 @@ public class PlayerCharacterBehaviour : MonoBehaviour
         // 방향 업데이트
         characterWorldAngle = Mathf.SmoothDampAngle(characterWorldAngle, characterWorldAngleTarget, ref characterWorldAngleSmooth, characterWorldAngleSmoothTime * (1.25f - Mathf.Min(moveMagnitude, 1)));
 
-        Debug.Log(characterWorldAngleSmoothTime * (1 - Mathf.Min(moveMagnitude, 1)));
+        //Debug.Log(characterWorldAngleSmoothTime * (1 - Mathf.Min(moveMagnitude, 1)));
         // 카메라가 바라보는 방향
         thisTransform.rotation = Quaternion.Euler(0, characterWorldAngle, 0);
     }
 
-    void LookAt(Vector2 direction)
+    private void OnCollisionEnter(Collision collision)
+    {
+        contactPoints.AddRange(collision.contacts);
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        contactPoints.AddRange(collision.contacts);
+    }
+
+    bool FindGround(out ContactPoint groundCP, List<ContactPoint> allCPs)
+    {
+        groundCP = default(ContactPoint);
+
+        if (contactPoints.Capacity == 0)
+        {
+            return true;
+        }
+
+        bool found = false;
+        foreach (ContactPoint cp in allCPs)
+        {
+            //Pointing with some up direction
+            if (cp.normal.y > 0.0001f && (found == false || cp.normal.y > groundCP.normal.y))
+            {
+                groundCP = cp;
+                found = true;
+            }
+        }
+
+        return found;
+    }
+
+    void SlopeUpdate()
+    {
+
+    }
+
+    //bool FindStep(out Vector3 stepUpOffset, List<ContactPoint> allCPs, ContactPoint groundCP)
+    //{
+    //    stepUpOffset = default;
+
+    //    //No chance to step if the player is not moving
+    //    var currVelocity = rigidbody.velocity;
+    //    Vector2 velocityXZ = new Vector2(currVelocity.x, currVelocity.z);
+    //    if (velocityXZ.sqrMagnitude < 0.0001f)
+    //        return false;
+
+    //    foreach (ContactPoint cp in allCPs)
+    //    {
+    //        bool test = ResolveStepUp(out stepUpOffset, cp, groundCP);
+    //        if (test)
+    //            return test;
+    //    }
+    //    return false;
+    //}
+
+    //bool ResolveStepUp(out Vector3 stepUpOffset, ContactPoint stepTestCP, ContactPoint groundCP)
+    //{
+    //    stepUpOffset = default;
+    //    Collider stepCol = stepTestCP.otherCollider;
+
+    //    //( 1 ) Check if the contact point normal matches that of a step (y close to 0)
+    //    if (Mathf.Abs(stepTestCP.normal.y) >= 0.01f)
+    //    {
+    //        return false;
+    //    }
+
+    //    //( 2 ) Make sure the contact point is low enough to be a step
+    //    if (!(stepTestCP.point.y - groundCP.point.y < maxStepHeight))
+    //    {
+    //        return false;
+    //    }
+
+    //    //( 3 ) Check to see if there's actually a place to step in front of us
+    //    //Fires one Raycast
+    //    RaycastHit hitInfo;
+    //    float stepHeight = groundCP.point.y + maxStepHeight + 0.0001f;
+    //    Vector3 stepTestInvDir = new Vector3(-stepTestCP.normal.x, 0, -stepTestCP.normal.z).normalized;
+    //    Vector3 origin = new Vector3(stepTestCP.point.x, stepHeight, stepTestCP.point.z) + (stepTestInvDir * stepSearchOvershoot);
+    //    Vector3 direction = Vector3.down;
+    //    if (!(stepCol.Raycast(new Ray(origin, direction), out hitInfo, maxStepHeight)))
+    //    {
+    //        return false;
+    //    }
+
+    //    //We have enough info to calculate the points
+    //    Vector3 stepUpPoint = new Vector3(stepTestCP.point.x, hitInfo.point.y + 0.0001f, stepTestCP.point.z) + (stepTestInvDir * stepSearchOvershoot);
+    //    Vector3 stepUpPointOffset = stepUpPoint - new Vector3(stepTestCP.point.x, groundCP.point.y, stepTestCP.point.z);
+
+    //    //We passed all the checks! Calculate and return the point!
+    //    stepUpOffset = stepUpPointOffset;
+    //    return true;
+    //}
+
+    void LookAtByCamera(Vector2 direction)
     {
         characterWorldAngleTarget = mainCameraTransform.rotation.eulerAngles.y + Mathf.Atan2(direction.y, -direction.x) * Mathf.Rad2Deg - 90;
     }
