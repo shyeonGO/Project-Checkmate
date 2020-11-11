e6edf7aa049f9b99e3f59bd7da0f7b390f6103f2
SteamB23 <steamb23@outlook.com>
2020-11-12 01:59:09 +0900

    플레이어 캐릭터 데이터 정리

--- Assets/Game/101. Scripts/PlayerCharacter/PlayerCharacterData.cs
+++ Assets/Game/101. Scripts/PlayerCharacter/PlayerCharacterData.cs
@@ -8,9 +8,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Player Character Data", menuName ="Game Data/Player Character Data")]
 public class PlayerCharacterData : ScriptableObject
 {
-    // double: 데이터
-    // float: 초
-
     [Tooltip("최대 체력")]
     public double MaxHp = 100;
 
@@ -24,29 +21,29 @@ public class PlayerCharacterData : ScriptableObject
     public double MaxStatePoint = 100;
 
     [Tooltip("피격시 무적 시간")]
-    public float NoDamageTimeByBeaten = 1;
+    public double NoDamageTimeByBeaten = 1;
 
     [Tooltip("스위칭 포인트 최소 요구량")]
     public double SwitchingPointsMinRequirements = 30;
 
     [Tooltip("스위칭 쿨타임")]
-    public float SwitchingCoolTime = 5;
+    public double SwitchingCoolTime = 5;
 
     [Tooltip("행동력 회복 딜레이")]
-    public float StaminaRecoveryDelay = 1;
+    public double StaminaRecoveryDelay = 1;
 
     [Tooltip("초당 행동력 회복량")]
     public double StaminaRecoveryPerSecond = 20;
 
     [Tooltip("전력 질주 행동력 소모량")]
-    public double SprintStaminaConsumption = 10;
+    public double StaminaConsumptionBySprint = 10;
 
     [Tooltip("회피 행동력 소모량")]
-    public double EvationStamianConsumption = 25;
+    public double StamianConsumptionByEvation = 25;
 
     [Tooltip("패링 스위칭 포인트 상승량")]
-    public double ParryingSwitchingPointProduction = 30;
+    public double SwitchingPointProductionByParrying = 30;
 
     [Tooltip("패링 그로기 포인트 상승량")]
-    public double ParryingGroggyPointProduction = 20;
+    public double GroggyPointProductionByParrying = 20;
 }
