41588d94051778dd00b5a4e316a31a9b2a45d21b
SteamB23 <steamb23@outlook.com>
2020-11-13 01:24:13 +0900

    Transform 기본 값으로 초기화하는 유틸리티 함수 추가

---
+++ Assets/Game/101. Scripts/Utility/ExtensionMethods/TransformExtension.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public static class TransformExtension
+{
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Initialize(this Transform transform)
+    {
+        transform.localPosition = default;
+        transform.localRotation = default;
+        transform.localScale = Vector3.one;
+    }
+}
