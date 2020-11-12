fb9a40a8b0ff7ede4d94edecbd27feba34001ecc
SteamB23 <steamb23@outlook.com>
2020-11-13 01:25:43 +0900

    플레이어 캐릭터 데미지 핸들러, 무기 데미지 트리거 구현

---
+++ Assets/Game/101. Scripts/PlayerCharacter/PlayerCharacterDamageHandler.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+class PlayerCharacterDamageHandler : DamageHandler
+{
+    [SerializeField] PlayerCharacterBehaviour behaviour;
+
+    private void Awake()
+    {
+        if (behaviour != null)
+        {
+            behaviour = GetComponent<PlayerCharacterBehaviour>();
+        }
+    }
+
+    public override void DamageHandle(double damage)
+    {
+        Debug.Log($"플레이어 데미지 핸들: {damage}");
+        behaviour.Status.Hp -= damage;
+    }
+}
--- Assets/Game/101. Scripts/PlayerCharacter/PlayerCharacterEquipment.cs
+++ Assets/Game/101. Scripts/PlayerCharacter/PlayerCharacterEquipment.cs
@@ -12,7 +12,16 @@ class PlayerCharacterEquipment : MonoBehaviour
     [Header("데이터")]
     [SerializeField] WeaponData weaponData;
 
+    PlayerCharacterBehaviour playerCharacterBehaviour;
+
     DamageTrigger[] damageTriggers = Array.Empty<DamageTrigger>();
+
+    private void Awake()
+    {
+        playerCharacterBehaviour = GetComponent<PlayerCharacterBehaviour>();
+
+        WeaponData = WeaponData;
+    }
 
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Correctness", "UNT0008:Null propagation on Unity objects", Justification = "<보류 중>")]
     public WeaponData WeaponData
@@ -22,30 +31,37 @@ class PlayerCharacterEquipment : MonoBehaviour
         {
             var weaponData = this.weaponData.ToReferenceNull();
             value = value.ToReferenceNull();
-            if (weaponData?.ID != value?.ID)
+
+            weaponData = value;
+
+            // 기존 무기 제거
+            if (currentWeaponTransform != null)
             {
-                weaponData = value;
+                //currentWeaponTransform.gameObject.SetActive(false);
+                var currentWeaponObject = currentWeaponTransform.gameObject;
 
-                // 기존 무기 제거
-                if (currentWeaponTransform != null)
+                damageTriggers = null;
+                Destroy(currentWeaponObject);
+            }
+
+            // 무기 오브젝트 생성
+            if (weaponData != null)
+            {
+                var currentWeaponObject = Instantiate(weaponData.WeaponPrefab);
+
+                // 부모 설정
+                currentWeaponTransform = currentWeaponObject.transform;
+                currentWeaponTransform.parent = weaponJoint;
+                currentWeaponTransform.Initialize();
+
+                damageTriggers = currentWeaponObject.GetComponents<DamageTrigger>();
+
+                foreach (var damageTrigger in damageTriggers)
                 {
-                    //currentWeaponTransform.gameObject.SetActive(false);
-                    var currentWeaponObject = currentWeaponTransform.gameObject;
-
-                    damageTriggers = null;
-                    Destroy(currentWeaponObject);
-                }
-
-                // 무기 오브젝트 생성
-                if (weaponData != null)
-                {
-                    var currentWeaponObject = Instantiate(weaponData.WeaponPrefab);
-
-                    // 부모 설정
-                    currentWeaponTransform = currentWeaponObject.transform;
-                    currentWeaponTransform.parent = weaponJoint;
-
-                    damageTriggers = currentWeaponObject.GetComponents<DamageTrigger>();
+                    if (damageTrigger is WeaponDamageTrigger weaponDamageTrigger)
+                    {
+                        weaponDamageTrigger.PlayerCharacterBehaviour = playerCharacterBehaviour;
+                    }
                 }
             }
         } // end of setter
