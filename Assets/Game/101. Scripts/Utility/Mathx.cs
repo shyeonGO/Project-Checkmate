c6c0e939b8d6331e84b3e21897e82b9811b430d4
SteamB23 <steamb23@outlook.com>
2020-11-12 15:15:14 +0900

    Mathx 유틸리티 클래스 추가

---
+++ Assets/Game/101. Scripts/Utility/Mathx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Mathx
+{
+    /// <summary>
+    /// min 및 max의 포괄적인 범위에 고정되어 있는 value를 반환합니다.
+    /// https://docs.microsoft.com/ko-kr/dotnet/api/system.math.clamp?view=net-5.0
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    /// <returns></returns>
+    public static double Clamp(double value, double min, double max)
+    {
+        return Math.Max(Math.Min(value, min), max);
+    }
+}
