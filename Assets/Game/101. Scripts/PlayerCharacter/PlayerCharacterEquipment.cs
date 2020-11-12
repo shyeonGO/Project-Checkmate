245f2040d5e58aa7758dc1406e8b8e0ffc8e0754
SteamB23 <steamb23@outlook.com>
2020-11-12 17:38:28 +0900

    PlayerCharacterEquipment에서 트리거 이벤트 받도록 수정

--- Assets/Game/101. Scripts/PlayerCharacter/PlayerCharacterEquipment.cs
+++ Assets/Game/101. Scripts/PlayerCharacter/PlayerCharacterEquipment.cs
@@ -1,16 +1,20 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using UnityEngine;
 
 class PlayerCharacterEquipment : MonoBehaviour
 {
     [Header("오브젝트 설정")]
-    [SerializeField] DamageTriggerManager damageTriggerManager;
+    //[SerializeField] DamageTriggerManager damageTriggerManager;
     [SerializeField] Transform weaponJoint;
     [SerializeField] Transform currentWeaponTransform;
 
     [Header("데이터")]
     [SerializeField] WeaponData weaponData;
 
+    DamageTrigger[] damageTriggers = Array.Empty<DamageTrigger>();
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Correctness", "UNT0008:Null propagation on Unity objects", Justification = "<보류 중>")]
     public WeaponData WeaponData
     {
         get => weaponData;
@@ -28,11 +32,7 @@ class PlayerCharacterEquipment : MonoBehaviour
                     //currentWeaponTransform.gameObject.SetActive(false);
                     var currentWeaponObject = currentWeaponTransform.gameObject;
 
-                    var damageTriggers = currentWeaponObject.GetComponents<DamageTrigger>();
-                    foreach(var damageTrigger in damageTriggers)
-                    {
-                        damageTriggerManager.DamageTriggers.Remove(damageTrigger);
-                    }
+                    damageTriggers = null;
                     Destroy(currentWeaponObject);
                 }
 
@@ -41,13 +41,39 @@ class PlayerCharacterEquipment : MonoBehaviour
                 {
                     var currentWeaponObject = Instantiate(weaponData.WeaponPrefab);
 
+                    // 부모 설정
                     currentWeaponTransform = currentWeaponObject.transform;
                     currentWeaponTransform.parent = weaponJoint;
 
-                    var damageTriggers = currentWeaponObject.GetComponents<DamageTrigger>();
-                    damageTriggerManager.DamageTriggers.AddRange(damageTriggers);
+                    damageTriggers = currentWeaponObject.GetComponents<DamageTrigger>();
                 }
             }
         } // end of setter
+    } // end of WeaponData
+
+    public void StartTrigger()
+    {
+        foreach (var damageTrigger in damageTriggers)
+        {
+            damageTrigger.StartTrigger();
+        }
+    }
+
+    public void EndTrigger()
+    {
+        foreach (var damageTrigger in damageTriggers)
+        {
+            damageTrigger.EndTrigger();
+        }
+    }
+
+    void DamageTrigger_StartTrigger()
+    {
+        StartTrigger();
+    }
+
+    void DamageTrigger_EndTrigger()
+    {
+        EndTrigger();
     }
 }
