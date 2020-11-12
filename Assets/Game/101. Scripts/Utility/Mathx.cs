e4ac3dde4d6ec7e0597582814c025554149dabee
SteamB23 <steamb23@outlook.com>
2020-11-12 15:52:31 +0900

    Mathx.Clamp 내부 구현 수정

--- Assets/Game/101. Scripts/Utility/Mathx.cs
+++ Assets/Game/101. Scripts/Utility/Mathx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +9,32 @@ public static class Mathx
 {
     /// <summary>
     /// min 및 max의 포괄적인 범위에 고정되어 있는 value를 반환합니다.
-    /// https://docs.microsoft.com/ko-kr/dotnet/api/system.math.clamp?view=net-5.0
     /// </summary>
+    /// <remarks>
+    /// 레퍼런스: https://docs.microsoft.com/ko-kr/dotnet/api/system.math.clamp?view=net-5.0
+    /// 소스코드: https://github.com/dotnet/runtime/blob/3642deea8235ce8316db8c37cc3e8f4ad5b15380/src/libraries/System.Private.CoreLib/src/System/Math.cs
+    /// </remarks>
     /// <param name="value"></param>
     /// <param name="min"></param>
     /// <param name="max"></param>
     /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static double Clamp(double value, double min, double max)
     {
-        return Math.Max(Math.Min(value, min), max);
+        if (min > max)
+        {
+            throw new ArgumentException("min이 max보다 큽니다.");
+        }
+
+        if (value < min)
+        {
+            return min;
+        }
+        else if (value > max)
+        {
+            return max;
+        }
+
+        return value;
     }
 }
