abcf3bf53072f3a264cf066646c20a1cff6045f0
lyp3238 <lyp3238@gmail.com>
2020-11-13 12:34:51 +0900

    AI 돌진 모션 적용 및 폴더정리 - 애니메이션이 세분화 되어있기에 따로 폴더를 만듬

--- Assets/Game/101. Scripts/AIPrototype/FSM/AttackEnter.cs
+++ Assets/Game/101. Scripts/AIPrototype/FSM/AttackEnter.cs
@@ -2,24 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackUpdate : StateMachineBehaviour
+public class AttackEnter : StateMachineBehaviour
 {
     private AIMaster aiMaster;
-    public bool continueAngleToPlayer = false;
+    public bool isSetAngleToPlayer = false;
 
-    // 애니메이션 구조로 볼 때 Enter, Update, Exit는 서로 나눠놓을 필요가 있어보임
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         aiMaster = animator.GetComponent<AIMaster>();
+        aiMaster.AttackSequence();
     }
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (continueAngleToPlayer)
+        if (isSetAngleToPlayer)
         {
-            aiMaster.SetEvadeDirection(true);
+            aiMaster.SetAngleToPlayer();
         }
     }
 }
