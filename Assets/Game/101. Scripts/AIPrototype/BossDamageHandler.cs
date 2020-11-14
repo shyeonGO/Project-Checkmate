344f67c87a55dba6c209dc73ffcb8844c7675dd3
lyp3238 <lyp3238@gmail.com>
2020-11-15 04:16:03 +0900

    Boss 데미지 핸들러 구현

---
+++ Assets/Game/101. Scripts/AIPrototype/BossDamageHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDamageHandler : DamageHandler
+{
+    [SerializeField]
+    private AIMaster aiMaster;
+
+    private void Awake()
+    {
+        if (aiMaster == null)
+        {
+            aiMaster = GetComponent<AIMaster>();
+        }
+    }
+
+    public override void DamageHandle(double damage)
+    {
+        Debug.Log($"Boss Damage Handle : {damage}");
+        aiMaster.healthPoint -= (float)damage;
+    }
+}
