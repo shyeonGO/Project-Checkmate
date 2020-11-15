5748fa7a01aa0eb623f409592d596402b72da71e
lyp3238 <lyp3238@gmail.com>
2020-11-16 00:36:28 +0900

    임시 보스 이펙트 적용 및 보스 내려찍기 공격 시 이펙트가 나타나도록 수정함 - 스크립트는 임시로 짜여져 있음

---
+++ Assets/Game/101. Scripts/AIPrototype/BossEffectManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEffectManager : MonoBehaviour
+{
+    public GameObject effectPrefab;
+    public Transform effectPosition;
+    public float effectDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Effect_Start()
+    {
+        GameObject effect = Instantiate(effectPrefab, effectPosition.position, Quaternion.identity);
+        StartCoroutine(DestroyParticle(effect));
+    }
+
+    IEnumerator DestroyParticle(GameObject gb)
+    {
+        yield return new WaitForSeconds(effectDuration);
+        Destroy(gb);
+    }
+}
