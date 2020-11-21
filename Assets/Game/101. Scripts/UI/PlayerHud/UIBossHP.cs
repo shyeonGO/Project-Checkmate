85211386a7f10e219c196d0a2d7f22d705a8219f
lyp3238 <lyp3238@gmail.com>
2020-11-22 03:55:04 +0900

    UIDesign 씬 변경 - UI 씬에서 -> AIPrototype 씬 기반으로 변경함

--- Assets/Game/101. Scripts/UI/PlayerHud/UIBossHP.cs
+++ Assets/Game/101. Scripts/UI/PlayerHud/UIBossHP.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class UIBossHP : UIHPBaseClass
 {
     public AIMaster aiMaster;
+    private BossDamageHandler damageEvent;
 
     protected override void Start()
     {
         maxHp = aiMaster.healthPoint;
         currentHp = maxHp;
+
+        damageEvent = aiMaster.GetComponent<BossDamageHandler>();
+        damageEvent.damageEvent.AddListener(UpdateHpBar);
     }
 
     protected override void SetCurrentHpBarSetting()
