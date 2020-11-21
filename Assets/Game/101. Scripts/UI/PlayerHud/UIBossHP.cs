3a881e71fb1fee2e87711fbb38aecdbede320041
lyp3238 <lyp3238@gmail.com>
2020-11-22 03:08:19 +0900

    보스에 임시로 HP바를 적용함

---
+++ Assets/Game/101. Scripts/UI/PlayerHud/UIBossHP.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBossHP : MonoBehaviour
+{
+    public AIMaster aiMaster;
+
+    public Image mainHpBar;
+    public Image subHpBar;
+    public float decreaseSpeed;
+
+    public double currentHp;
+    public double maxHp;
+
+    private void Start()
+    {
+        maxHp = aiMaster.healthPoint;
+    }
+
+    public void UpdateHpBar()
+    {
+        currentHp = aiMaster.healthPoint;
+        mainHpBar.fillAmount = (float)(currentHp / maxHp);
+        StartCoroutine(LerpSubHpBar());
+    }
+
+    IEnumerator LerpSubHpBar()
+    {
+        while (Mathf.Abs(mainHpBar.fillAmount - subHpBar.fillAmount) > 0.001f)
+        {
+            subHpBar.fillAmount = Mathf.Lerp(subHpBar.fillAmount, mainHpBar.fillAmount, Time.deltaTime * decreaseSpeed);
+            yield return null;
+        }
+    }
+}
